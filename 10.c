976f365dd353dfb5d3865e9533a849baefcb437e
wongsyrone <wong.syrone@gmail.com>
2014-04-21 21:20:19 +0800

    10.c: add proper interrupt version of dynamic 7-segment display. Big Thanks to Fancyguy

--- 10.c
+++ 10.c
@@ -1,19 +1,143 @@
 /*
+	Author: Fancyguy  wongsyrone
+	Big Thanks to Fancyguy for contributing to judgement part.
+		He widen my horizon.
+	And, I will attach my previous version of this for comparison.
+	
+	
 	This is the INTERRUPT version, we using timer interrupt in the 8051 mcu
 		to make it accurate when display something
 	The interrupt part only have timer initialization, and put other statement
-		into main() or sub-functions
+		into sub-functions display()
 	
 	something we should know
 	- EA...interrupt enable
 	- ET0... Timer 0 
 	
 	TODO:
-	- abandon FLAG part for check purpose   ... maybe we will find a better way
-	- display part
-			How to change screen to another one
+	- 
+	
+	IMPORTANT: judgement statements part by Fancyguy
 
 */
+
+#include <reg52.h>
+#define uint unsigned int
+#define uchar unsigned char
+
+sbit dula=P2^6;   //  what to show
+sbit wela=P2^7;   //  which one to show
+
+/*
+	define what we will display 
+	screen1...11-d-1
+	screen2...050118
+	choice...from left to right, for dynamic display purpose only
+*/
+uchar code screen1[]={0x06,0x06,0x40,0x5e,0x40,0x06};
+uchar code screen2[]={0x3f,0x6d,0x3f,0x06,0x06,0x7f};
+uchar code choice[]={0xfe,0xfd,0xfb,0xf7,0xef,0xdf};
+
+uchar num;
+
+void delayms (uint ms)
+{
+	uint i,j;
+	for (i=ms; i>0; i--)
+		for (j=110; j>0; j--);
+}
+
+/*
+        0     1000     2000   3000  | 4000
+scr     1       1        0      0   |   1      
+switch  1       0        1      0   |   1
+num     0       20       40     60  |   80 
+*/
+void T0_timer() interrupt 1
+{
+	// 1 sec
+	TH0=(65536-45872)/256;
+	TL0=(65536-45872)%256;
+	num++;
+	if (num == 80)  // 4 sec
+		num = 0;
+}
+
+void display ()
+{
+	uchar i,j;
+	while (num <= 20)  				//to keep lighting for the fisrt 1s
+	{
+		for (i=0; i<6; i++)
+		{
+			dula=1;
+			P0=screen1[i];
+			dula=0;
+			P0=0xff;
+			wela=1;
+			P0=choice[i];
+			wela=0;
+			delayms(1);
+		}
+	}		
+	while(num <= 40 && num > 20)			 // to shut down for the first 1s
+	{
+		dula=1;
+		P0=0x00;
+		dula=0;
+	}
+
+	while (num <= 60)    	 		// to keep lighting for the second 1s
+	{
+		for (j=0; j<6; j++)
+		{
+			dula=1;
+			P0=screen2[j];
+			dula=0;
+			P0=0xff;
+			wela=1;
+			P0=choice[j];
+			wela=0;
+			delayms(1);
+		}
+	}
+	while(num <= 80 && num > 60) 		//to shut down for the second 1s
+	{
+		dula=1;
+		P0=0x00;
+		dula=0;
+	}
+}
+
+void init (void)
+{
+	TMOD=0x01;  // timer 0 method 1
+	TH0=(65536-45872)/256; // initialize timer 0
+	TL0=(65536-45872)%256;
+	EA=1;   // open all interrupt
+	ET0=1;  // setup timer 0
+	TR0=1; //start timer 0
+	num=0;
+}
+
+void main (void)
+{
+	init();
+	while(1)
+	{
+		display();
+	}
+}
+
+
+///////////////////////////////////////////////////////////////////////
+/////////////////
+////////////////          My Previous Version
+///////////////
+/////////////
+/////////                Just for Comparison
+////////
+////////////////////////////////////////////////////////////////////////
 
 #include <reg52.h>
 #define uint unsigned int
@@ -26,121 +150,117 @@ sbit wela=P2^7;   //  which one to show
 initial flag, which means show something  
 op_scr=1 ... display screen 1; op_scr=0 ... display screen 2
 op_switch=1 ... show ; op_switch=0 ... clear
-
-
+*/
 bit op_scr=1;
 bit op_switch=1;
-*/
-bit op;
+
 /*
 	define what we will display 
 	screen1...11-d-1
 	screen2...050118
 	choice...from left to right, for dynamic display purpose only
-		uchar code choice[]={0xfe,0xfd,0xfb,0xf7,0xef,0xdf}
 */
-
 uchar code screen1[]={0x06,0x06,0x40,0x5e,0x40,0x06};
 uchar code screen2[]={0x3f,0x6d,0x3f,0x06,0x06,0x7f};
 uchar code choice[]={0xfe,0xfd,0xfb,0xf7,0xef,0xdf};
 
-uchar num_t0;
+uchar num;
 
-void delayms (uint ms)
-{
-	uint i,j;
-	for (i=ms; i>0; i--)
-		for (j=110; j>0; j--);
-}
+//void delayms (uint ms)
+//{
+//	uint i,j;
+//	for (i=ms; i>0; i--)
+//		for (j=110; j>0; j--);
+//}
 
 /*
-ABANDON
-        0     1000     2000   3000   4000
-scr           
-switch  
-num     0       20       40     60     80 
+        0     1000     2000   3000  | 4000
+scr     1       1        0      0   |   1      
+switch  1       0        1      0   |   1
+num     0       20       40     60  |   80 
 */
 void T0_timer() interrupt 1
 {
 	// 1 sec
-	TH0=(65536-45872)/256;  //50 ms
+	TH0=(65536-45872)/256;
 	TL0=(65536-45872)%256;
-	num_t0++;
-	if (num_t0 ==20)
+	num++;
+	if (num == 20 || num == 40 || num == 60)
 	{
-		wela=1;
-		P0=0xff;
-		wela=0;
-		
-		if (num_t0 == 40)
+		op_switch=~op_switch;
+		if (num == 40)
 		{
-			op=0;
-			if (num_t0 == 60)
-			{
-				wela=1;
-				P0=0xff;
-				wela=0;
-				
-				if (num_t0 == 80)
-				{
-					op=1;
-					num_t0=0;
-				}
-			}
+			op_scr=~op_scr;
+			if (num == 80)  // 4 sec
+				num=0;
 		}
 	}
 }
 
-void display (bit op)
+void show_screen1 (void)
 {
-	uchar i;
-	if (op == 1)
+	uchar a,j;
+	while (op_scr == 1)
 	{
-		for (i=0; i<6; i++)
+	for (a=0; a<6; a++)
 		{
 			dula=1;
-			P0=screen2[i];
+			P0=screen1[a];
 			dula=0;
 			P0=0xff;
 			wela=1;
-			P0=choice[i];
+			P0=choice[a];
 			wela=0;
-			delayms(50);
+			for (j=100; j>0; j--);
 		}
 	}
-	else
-	{
-		for (i=0; i<6; i++)
-		{
-			dula=1;
-			P0=screen1[i];
-			dula=0;
-			P0=0xff;
-			wela=1;
-			P0=choice[i];
-			wela=0;
-			delayms(50);
-		}
-	}
+
 }
 
-void init (void)
+void show_screen2 (void)
 {
-	TMOD=0x01;  // timer 0 method 1 
+	uchar b,j;
+	while (op_scr == 0)
+	{
+	for (b=0; b<6; b++)
+		{
+			dula=1;
+			P0=screen2[b];
+			dula=0;
+			P0=0xff;
+			wela=1;
+			P0=choice[b];
+			wela=0;
+			for (j=100; j>0; j--);
+		}
+	}
+
+}
+
+void main (void)
+{
+	TMOD=0x01;  // timer 0 method 1
 	TH0=(65536-45872)/256; // initialize timer 0
 	TL0=(65536-45872)%256;
 	EA=1;   // open all interrupt
 	ET0=1;  // setup timer 0
 	TR0=1; //start timer 0
-	num_t0=0;
-	op=1;
-}
-
-void main (void)
-{
-	init();
+	num=0;
 	while(1)
 	{
-		display(op);
+		show_screen1();
+		while (op_switch == 0)
+		{
+			wela=1;
+			P0=0xff;
+			wela=0;
+		}
+		show_screen2();
+		while (op_switch == 0)
+		{
+			wela=1;
+			P0=0xff;
+			wela=0;
+		}
 	}
 }
